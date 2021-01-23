2e5103a96d96ec515c0dac149bf0046003fc71a0
David Strauch <dmaster18@gmail.com>
2021-01-23 23:41:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import manageUsers from './reducers/manageUsers';
+import App from './App'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
-// we wrap store in a function for testing purposes
-export const configureStore = () => {
-<<<<<<< HEAD
-return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-=======
-  return createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
->>>>>>> eb6bb67da8fb827982339f8511b35ab7b479ceaa
-};
 
-let store = configureStore();
+import manageUsers from './reducers/manageUsers'
+
+
+// add imports and code
+const store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
 
 ReactDOM.render(
-<<<<<<< HEAD
-<Provider store={store}>
-  <App store={store}/>
-</Provider>,
-document.getElementById('root')
-=======
-  <Provider store={store}>
-    <App store={store}/>
-  </Provider>,
+  // add imports and code
+  <Provider store = {store}>
+      <App />
+  </Provider>
+
+  // add imports and code
+  ,
   document.getElementById('root')
->>>>>>> eb6bb67da8fb827982339f8511b35ab7b479ceaa
 );
