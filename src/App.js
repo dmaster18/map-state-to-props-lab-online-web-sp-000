f33b83a61930c3e4b40f8562e93550eaff5c1d01
David Strauch <dmaster18@gmail.com>
2021-01-23 23:41:11 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import UserInput from './components/UserInput';
-import { ConnectedUsers } from './components/Users';
-=======
 import UserInput from './components/UserInput'
+import ConnectedUsers from './components/Users'
 
-import { ConnectedUsers } from './components/Users'
->>>>>>> eb6bb67da8fb827982339f8511b35ab7b479ceaa
-
-export class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserInput store={this.props.store}/>
+        <UserInput />
         <ConnectedUsers />
       </div>
     );
   }
 }
 
-export default App;
+export default App
